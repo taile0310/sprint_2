4a5f4b8f4de1d36cd7ed31b657dcf9ae9a6adbef
lehuutai0310 <lehuutai0310@gmail.com>
2023-04-14 19:51:48 +0700

    TaiLH update prototype

--- BE/sprint2/src/main/java/com/example/sprint2/Sprint2Application.java
+++ BE/sprint2/src/main/java/com/example/sprint2/Sprint2Application.java
@@ -8,6 +8,25 @@ public class Sprint2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Sprint2Application.class, args);
+//        for (int i = 0; i < 100; i++) {
+//            int fib = fibonacci(i);
+//            if (fib >= 100) {
+//                break;
+//            }
+//            System.out.print(fib + " ");
+//        }
     }
+
+
+//        public static int fibonacci(int n) {
+//            if (n == 0) {
+//                return 0;
+//            } else if (n == 1) {
+//                return 1;
+//            } else {
+//                return fibonacci(n - 1) + fibonacci(n - 2);
+//            }
+//        }
+
 
 }
